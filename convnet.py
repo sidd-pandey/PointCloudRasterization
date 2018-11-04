88b59ae5e2f67c7c63afac9bc367d60e47946b73
Siddharth Pandey <sidd.pandey95@gmail.com>
2018-11-04 12:35:53 +0800

    convent

---
+++ convnet.py
@@ -0,0 +1,55 @@
+import sklearn.metrics
+import keras.utils
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import SGD
+from utils import read_data, plot_history
+from keras.utils import np_utils
+from config import *
+
+keras.backend.set_image_data_format('channels_first')
+X_train, Y_train = read_data(TRAIN_PATH, NUM_POINTS)
+X_test, Y_test = read_data(TEST_PATH, NUM_POINTS)
+labels = np_utils.to_categorical(Y_train, num_classes=NUM_CLASSES)
+
+print(X_train.shape)
+print(Y_train.shape)
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(6, 64, 64)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(40, activation='softmax'))
+
+model.compile(optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
+
+model.summary()
+
+early_stopping = EarlyStopping(monitor="val_loss", patience=10)
+model_checkpoint = ModelCheckpoint("saved_models/best_model.h5", save_best_only=True, verbose=1)
+history = model.fit(X_train, labels, epochs=1, batch_size=512,
+    validation_split=0.2, callbacks=[early_stopping, model_checkpoint])
+
+test_labels = np_utils.to_categorical(Y_test, num_classes=NUM_CLASSES)
+
+score = model.evaluate(x=X_test, y=test_labels)
+print("test loss & accuracy: ", score)
+
+plot_history(history)
+keras.utils.plot_model(model, to_file="saved_models/model_desc.png", show_shapes=True)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from keras.layers import Dense, Activation, Dropout
 from keras.utils import np_utils
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-# def main():
+
 X_train, Y_train = read_data(TRAIN_PATH, NUM_POINTS)
 X_test, Y_test = read_data(TEST_PATH, NUM_POINTS)
 labels = np_utils.to_categorical(Y_train, num_classes=40)
@@ -37,13 +37,10 @@ history = model.fit(X_train.reshape(-1, 64 * 64 * 6), labels, epochs=50, batch_s
     validation_split=0.1, callbacks=[early_stopping, model_checkpoint])
 
 test_labels = np_utils.to_categorical(Y_test, num_classes=40)
-test_pred = model.predict(X_test.reshape(-1, 64 * 64 *6))
-test_pred = np_utils.to_categorical(test_pred.argmax(axis=-1), 40)
 
-print("test accuracy: ",sklearn.metrics.accuracy_score(test_labels, test_pred))
+score = model.evaluate(x=X_test.reshape(-1, 64 * 64 *6), y=test_labels)
+
+print("test loss & accuracy: ", score)
+
 plot_history(history)
-
-
-
-# if __name__ == "__main__":
-#     main()
+keras.utils.plot_model(model, to_file="saved_models/model_desc.png", show_shapes=True)
